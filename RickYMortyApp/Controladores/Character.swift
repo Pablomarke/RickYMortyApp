909de225fd27a8a7dd345fa6a99005884a4439e7
Pablo Márquez Marín <pablomarquezartesano@gmail.com>
2022-10-31 12:40:14 +0100

    limpieza codigo

--- RickYMortyApp/Controladores/Character.swift
+++ RickYMortyApp/Controladores/Character.swift
@@ -10,9 +10,13 @@ import Foundation
 // MARK: - Character
 struct Character: Codable {
     let id: Int
-    let name, status, species, type: String
+    let name: String
+    let status: String
+    let species: String
+    let type: String
     let gender: String
-    let origin, location: Location
+    let origin: Location
+    let location: Location
     let image: String
     let episode: [String]
     let url: String
--- RickYMortyApp/Controladores/Episode.swift
+++ RickYMortyApp/Controladores/Episode.swift
@@ -10,14 +10,20 @@ import Foundation
 // MARK: - Episode
 struct Episode: Codable {
     let id: Int
-    let name, airDate, episode: String
+    let name: String
+    let  airDate: String
+    let episode: String
     let characters: [String]
     let url: String
     let created: String
-
+    
     enum CodingKeys: String, CodingKey {
-        case id, name
+        case id
+        case name
         case airDate = "air_date"
-        case episode, characters, url, created
+        case episode
+        case characters
+        case url
+        case created
     }
 }

c1e7bc84ea6955957e01ef1dab01d0302f155b83
Pablo Márquez Marín <pablomarquezartesano@gmail.com>
2022-12-03 15:15:19 +0100

    creación HomeView controller e implementado

--- RickYMortyApp/AppDelegate.swift
+++ RickYMortyApp/AppDelegate.swift
@@ -15,7 +15,7 @@ var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        window?.rootViewController = PruebaViewController()
+        window?.rootViewController = HomeViewController()
         return true
     }
 
